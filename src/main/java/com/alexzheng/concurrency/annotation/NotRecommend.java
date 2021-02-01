e9dcd9b3ff439efe8a028390907ade999e2e8469
AlexZheng <zc_work@outlook.com>
2021-02-02 07:24:03 +0800

    1.使用创建四个标记注解，用于后续使用。

---
+++ src/main/java/com/alexzheng/concurrency/annotation/NotRecommend.java
@@ -0,0 +1,21 @@
+package com.alexzheng.concurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author Alex Zheng
+ * @Date 2021/2/2 7:14
+ * @Annotation 不推荐的类或写法
+ */
+//下面指我们注解的目标 ElementType类可以单独研究一下
+@Target(ElementType.TYPE)
+//下面注解表示该注解只在源码存在
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+    String value() default "";
+
+}
---
+++ src/main/java/com/alexzheng/concurrency/annotation/ThreadSafe.java
@@ -0,0 +1,21 @@
+package com.alexzheng.concurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author Alex Zheng
+ * @Date 2021/2/2 7:14
+ * @Annotation 用于标记线程安全的类或写法
+ */
+//下面指我们注解的目标 ElementType类可以单独研究一下
+@Target(ElementType.TYPE)
+//下面注解表示该注解只在源码存在
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+
+    String value() default "";
+
+}
