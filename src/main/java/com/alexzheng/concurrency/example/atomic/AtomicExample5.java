cb4d1c0d7f2d8b0b522ecbc02cb46f30f7daa394
AlexZheng <zc_work@outlook.com>
2021-02-02 10:42:16 +0800

    AtomicReferenceFieldUpdater

---
+++ src/main/java/com/alexzheng/concurrency/example/atomic/AtomicExample5.java
@@ -0,0 +1,39 @@
+package com.alexzheng.concurrency.example.atomic;
+
+import com.alexzheng.concurrency.annotation.ThreadSafe;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
+/**
+ * @Author Alex Zheng
+ * @Date 2021/2/2 7:43
+ * @Annotation AtomicReferenceFieldUpdater------ || AtomicIntegerFieldUpdater
+ */
+@Slf4j
+@ThreadSafe
+public class AtomicExample5 {
+
+    public static AtomicIntegerFieldUpdater<AtomicExample5> updater =
+            AtomicIntegerFieldUpdater.newUpdater(AtomicExample5.class,"count");
+
+    @Getter
+    public volatile int count = 100;
+
+    public static void main(String[] args) {
+        AtomicExample5 example5 = new AtomicExample5();
+        if (updater.compareAndSet(example5, 100, 120)) {
+            log.info("update success 1, {}", example5.getCount());
+        }
+
+        if (updater.compareAndSet(example5, 100, 120)) {
+            log.info("update success 2, {}", example5.getCount());
+        } else {
+            log.info("update failed, {}", example5.getCount());
+        }
+    }
+
+}
